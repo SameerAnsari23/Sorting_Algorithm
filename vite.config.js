64446fdc95bc6cc11e79396e405301b4ab462abc
SameerAnsari23 <sameeransariscience@gmail.com>
2025-04-09 09:39:36 +0530

    Commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
-import { defineConfig } from "vite";
-import reactRefresh from "@vitejs/plugin-react-refresh";
+// import { defineConfig } from "vite";
+// import reactRefresh from "@vitejs/plugin-react-refresh";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [reactRefresh()],
-  build: {
-    target: "es2018",
-  },
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [reactRefresh()],
+//   build: {
+//     target: "es2018",
+//   },
   
+// });
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    target: 'es2018',
+  },
 });
+
